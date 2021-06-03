450e1fd95067a024cfb4d0d2f9ff1a71219b2c61
Павел Стерехов <iiabejic@gmail.com>
2021-06-03 18:18:53 +0500

    Fixed

--- homework.py
+++ homework.py
@@ -1,9 +1,12 @@
 import os
 import time
 import logging
+from json import JSONDecodeError
 
 import requests
 from dotenv import load_dotenv
+from requests.exceptions import Timeout, SSLError, ConnectionError
+from requests.exceptions import RequestException
 from telegram import Bot
 
 load_dotenv()
@@ -13,32 +16,34 @@ PRAKTIKUM_TOKEN = os.getenv("PRAKTIKUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 PRAKTIKUM_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+STATUS_DATA = {'rejected': 'К сожалению в работе нашлись ошибки.',
+               'reviewing': 'Ревьюер взял работу на проверку.',
+               'approved': ('Ревьюеру всё понравилось, ' 
+                            'можно приступать к следующему уроку.')}
+HEADERS = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
 
 
 def parse_homework_status(homework):
     homework_name = homework['homework_name']
-    if homework['status'] == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    elif homework['status'] == 'reviewing':
-        verdict = 'Ревьюер взял работу на проверку.'
-    else:
-        verdict = ('Ревьюеру всё понравилось, '
-                   'можно приступать к следующему уроку.')
+    status = homework['status']
+    if status not in STATUS_DATA:
+        raise ValueError(f'Несуществующий статус проекта - {status}')
+    verdict = STATUS_DATA[status]
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
-    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     date = {'from_date': current_timestamp}
     try:
-        homework_statuses = requests.get(
+        response = requests.get(
             PRAKTIKUM_URL,
-            headers=headers,
+            headers=HEADERS,
             params=date
         )
-        return homework_statuses.json()
-    except requests.RequestException as e:
-        logging.error(e, exc_info=True)
+    except (Timeout, SSLError, ConnectionError, OSError,
+            RequestException, JSONDecodeError):
+        raise
+    return response.json()
 
 
 def send_message(message, bot_client):
@@ -48,13 +53,6 @@ def send_message(message, bot_client):
 def main():
     bot = Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
-    logging.basicConfig(
-        level=logging.DEBUG,
-        filemode='w',
-        filename='bot.log',
-        format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
-    )
-
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
@@ -63,10 +61,6 @@ def main():
                     new_homework.get('homeworks')[0]
                 )
                 send_message(message, bot)
-                send_message(
-                    parse_homework_status(new_homework.get('homeworks')[0]),
-                    bot
-                )
                 logging.info(f'Сообщение отправлено:\n«{message}»')
             current_timestamp = new_homework.get(
                 'current_date',
@@ -74,11 +68,16 @@ def main():
             )
             time.sleep(1200)
 
-        except Exception as e:
-            message = f'Бот столкнулся с ошибкой: {e}'
-            logging.error(message, exc_info=True)
+        except Exception as error:
+            logging.error(f'Бот столкнулся с ошибкой: {error}', exc_info=True)
             time.sleep(1200)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filemode='w',
+        filename=__file__ + '.log',
+        format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+    )
     main()
